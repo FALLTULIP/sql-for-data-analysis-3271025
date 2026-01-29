d880a971ff4794ff455ca304d689618adc9ce712
FALLTULIP <159815771+FALLTULIP@users.noreply.github.com>
2026-01-29 19:54:13 +0000

    My edits

--- Chapter 2/Finding Inaccurate Data Values/CheckOrderTotal.sql
+++ Chapter 2/Finding Inaccurate Data Values/CheckOrderTotal.sql
@@ -1,8 +1,9 @@
 SELECT OrderItem.OrderId,
   Orders.TotalDue,
-  SUM(Price * Quantity) as NewTotalDue
+  ROUND(SUM(Price * Quantity),2) as NewTotalDue
 FROM OrderItem
   JOIN Product ON OrderItem.ProductID = Product.ProductID
   JOIN Orders ON OrderItem.OrderID = Orders.OrderID
 GROUP BY OrderItem.OrderId,
   Orders.TotalDue
+HAVING Orders.TotalDue < ROUND(SUM(Price * Quantity),2); 
