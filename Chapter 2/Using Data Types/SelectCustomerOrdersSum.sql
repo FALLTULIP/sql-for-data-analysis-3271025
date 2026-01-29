f6123e78101c95f4b7694d4bc75edfebe433a802
FALLTULIP <159815771+FALLTULIP@users.noreply.github.com>
2026-01-29 19:34:07 +0000

    Adding Round

--- Chapter 2/Using Data Types/SelectCustomerOrdersSum.sql
+++ Chapter 2/Using Data Types/SelectCustomerOrdersSum.sql
@@ -1,7 +1,7 @@
 SELECT Customer.CustomerID,
   FirstName,
   LastName,
-  Sum(TotalDue)
+  ROUND(Sum(TotalDue), 2)
 From Customer
   JOIN Orders on Customer.CustomerID = Orders.CustomerID
 GROUP BY Customer.CustomerID,
